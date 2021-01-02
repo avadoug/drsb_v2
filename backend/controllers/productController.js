b67f076b546f281c6a2e64c9d1afafd50582b852
avadoug <avadoug@gmail.com>
2021-01-01 22:45:25 -0500

    Update productController.js

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -171,7 +171,7 @@ const createProductReview = asyncHandler(async (req, res) => {
 // @route   GET /api/products/top
 // @access  Public
 const getTopProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({}).sort({ rating: -1 }).limit(3)
+  const products = await Product.find({}).limit(3)
 
   res.json(products)
 })
